cf08c93a7ce9bb243d2eda4d103652830bc26d7c
sutt <wsutton17@gmail.com>
2018-02-04 15:13:41 -0500

    adding a clean workspace to wrapping opencfu

--- utils.py
+++ utils.py
@@ -3,6 +3,41 @@ from PIL import Image
 import cv2
 import imutils
 
+PATH_IMG = "data/images/"
+PATH_COLONY_DATA = "data/colony-data/"
+PATH_IMG_TMP = "data/images-tmp/"
+
+
+def build_img_path_name(img_ext=".jpg"):
+	img_name = str(time.time())						#TODO - bettr filename
+	img_name += img_ext
+	img_path_name = PATH_IMG + img_name
+	return img_path_name
+
+def capture_image(img_path_name, b_verbose=False):
+	cmd = ["raspistill", "-o", img_path_name]
+	if b_verbose:
+		cmd.exntend(["-v"])
+	out = subprocess.check_output(cmd)
+	return out
+	
+def load_image(img_path_name):
+	img = Image.open(img_path_name)
+	return img
+
+def crop_img(img, b_save_to_tmp = True):
+	img_crop = img.crop((0,0,400,400))
+	if b_save_to_tmp:
+		tmp_img_fn = PATH_IMG_TMP + "tmp.jpg"		#TODO - remove hard-code path
+		im2.save(tmp_img_fn)		
+	return img2
+	
+def run_opencfu(input_img_path_name, b_filesystem=True):
+	cmd = ["opencfu", "-i", input_img_path_name]
+	if b_filesystem:
+		cmd.extend([">", path_to_colony_data])	#pipe to data/colony-data/
+	output_text = subprocess.check_output(cmd)
+	return output_text
 
 def is_float(x):
     return str(float(x)) == x
@@ -20,6 +55,8 @@ def import_data(data_fn):
 	data_type = lines[1]
 	data = lines[1:]
 	
+	#all data is an int or float,
+	#use type in first row, as type for whole column
 	data_type = map(lambda x: is_float(x), data_type)
 
 	d = {}
@@ -28,7 +65,7 @@ def import_data(data_fn):
 		for i_row, row in enumerate(data):
 			try:
 				s_v = row[i_k]
-				v = float(s_v) if is_float(s_v) else int(s_v)
+				v = float(s_v) if data_type[i] else int(s_v)
 				d[k].append(v)
 			except:
 				print 'error parsing data elem ', str(k), ' line ', str(i_row)
@@ -59,21 +96,38 @@ def display_img(img, mod_=2, b_resize=True):
 							)
 							
 	cv2.imshow('annotated img', img)
-	if cv2.waitKey(0)== ord('q'):
+	if cv2.waitKey(0) == ord('q'):
 		return True
+		
+#TODO - add a way to run an image server locally
+#def run_image_server():
+#	out = subprocess.call(["python" , "-m", "SimpleHTTPServer"])
+#	print out	#so we know what port to connect on
+	
 		
 def main():
 	
 	data_fn = "data/colony-data/sample1.csv"
 	img_fn = "data/samples/sample1.jpg"
 	
+	cap_img_fn = build_img_path_name()
+	capture_image(cap_img_fn)
+	
+	cap_img = load_image(cap_img_fn)
+	
+	crop_img = crop_img(cap_img)
+	save_img(crop_img)
+	
+	#run_opencfu(crop_img_fn)	#TODO add crop_img_fn
+	
 	d = import_data(data_fn)
-	img = cv2.imread(img_fn)
+	
+	img_cv = cv2.imread(img_fn)
 	
 	x, y, radius = d['X'], d['Y'], d['Radius']	
-	img = draw_colonies(img, x, y, radius, max_colonies=0)
+	drawn_img = draw_colonies(img_cv, x, y, radius, max_colonies=0)
 	
-	display_img(img)
+	display_img(drawn_img)
 	
 	print 'exiting main'
 	
